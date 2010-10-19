8af8e30bd623eac6c99f347fbbb139cd23a5b3d1
Ben Webb <ben@salilab.org>
2010-10-19 18:50:29 +0000

    Add help, page border, greeting.

--- lib/salign.pm
+++ lib/salign.pm
@@ -18,15 +18,46 @@ sub get_navigation_links {
 }
 
 sub get_project_menu {
-    # TODO
+    my $self = shift;
+    my $version = $self->version;
+    return <<MENU;
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<h4><small>Developers:</small></h4>
+<p>
+Hannes Braberg<br />
+Mallur S. Madhusudhan<br />
+Ursula Pieper<br />
+Ben Webb<br />
+Andrej Sali</p>
+<p><i>Version $version</i></p>
+MENU
 }
 
 sub get_footer {
-    # TODO
+    my $self = shift;
+    my $htmlroot = $self->htmlroot;
+    return <<FOOTER;
+<div id="address">
+<p>Please cite: Hannes Braberg, Mallur S. Madhusudhan, Ursula Pieper,
+Ben Webb, Andrej Sali. SALIGN : A multiple protein structure/sequence
+alignment web server. In preparation.</p>
+</div>
+FOOTER
 }
 
 sub get_index_page {
-    # TODO
+    my $self = shift;
+    my $q = $self->cgi;
+    my $greeting = <<GREETING;
+<p>SALIGN is a general alignment module of the modeling program
+<a href="http://salilab.org/modeller/">MODELLER</a>.</p>
+
+<p>The alignments are computed using dynamic programming, making use of
+several features of the protein sequences and structures.</p>
+GREETING
+  return $greeting;
 }
 
 sub get_submit_page {
