4bddc463f0ba057da55efd5fe4d3bb7e3ffedd35
Ben Webb <ben@salilab.org>
2010-11-25 02:05:50 +0000

    Basic implementation of script generation for seq-seq jobs.

---
+++ test/backend/test_job.py
@@ -0,0 +1,11 @@
+import unittest
+import salign
+import saliweb.test
+
+class JobTests(saliweb.test.TestCase):
+    def test_run(self):
+        """Test run method"""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
