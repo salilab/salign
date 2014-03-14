d819d0d4698f2f8697b72b103c3906d71ec59652
Ben Webb <ben@salilab.org>
2014-03-14 23:39:53 +0000

    Add tests for new backend.

--- test/backend/test_job.py
+++ test/backend/test_job.py
@@ -1,11 +1,28 @@
 import unittest
 import salign
 import saliweb.test
+import anydbm
 
 class JobTests(saliweb.test.TestCase):
     def test_run(self):
         """Test run method"""
-        pass
+        for tool in ('str_str', '1s_sese', '2s_sese', 'str_seq'):
+            j = self.make_test_job(salign.Job, 'RUNNING')
+            d = saliweb.test.RunInDir(j.directory)
+            db = anydbm.open('inputs.db', 'n')
+            db['tool'] = tool
+            db.close()
+            j.run()
+            del d
+
+    def test_run_bad_tool(self):
+        """Test run method with bad tool"""
+        j = self.make_test_job(salign.Job, 'RUNNING')
+        d = saliweb.test.RunInDir(j.directory)
+        db = anydbm.open('inputs.db', 'n')
+        db['tool'] = 'garbage'
+        db.close()
+        self.assertRaises(ValueError, j.run)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ test/backend/test_utils.py
@@ -0,0 +1,32 @@
+import unittest
+import salign
+import saliweb.test
+import saliweb.backend
+
+class DummyRunner(object):
+    def __init__(self, script): self.script = script
+    def set_sge_options(self, opts): self.opts = opts
+
+class Tests(saliweb.test.TestCase):
+
+    def test_make_sge_script(self):
+        """Check make_sge_script()"""
+        r = salign.make_sge_script(DummyRunner, ["foo", "bar"])
+
+    def test_run_mod(self):
+        """Check run_mod() function"""
+        m = salign.run_mod('test.py')
+        self.assertEqual(m, 'python test.py > test.log')
+
+    def test_make_onestep_script(self):
+        """Check make_onestep_script()"""
+        s = salign.make_onestep_script(DummyRunner, 'str_str')
+        s = salign.make_onestep_script(DummyRunner, 'seq_seq')
+
+    def test_make_twostep_script(self):
+        """Check make_twostep_script()"""
+        s = salign.make_twostep_script(DummyRunner, 'str_seq')
+        s = salign.make_twostep_script(DummyRunner, 'seq_seq')
+
+if __name__ == '__main__':
+    unittest.main()
