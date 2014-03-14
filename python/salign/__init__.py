75ac034a41768d779c8d84ae95dbc94033fb1e23
Ben Webb <ben@salilab.org>
2014-03-14 23:25:58 +0000

    Make SGE scripts for new submissions.

--- python/salign/__init__.py
+++ python/salign/__init__.py
@@ -1,183 +1,65 @@
 import saliweb.backend
-import yaml
+import anydbm
 import os
-import bsddb.db
 
-def read_parameters_file(fh):
-    parameters = yaml.safe_load(fh)
-    return parameters
-
-def sese_stse_topf(inputs, ali_files, seq_count, top_type, output_ali):
-    """Create script for seq-seq and str-seq"""
-    script = """
-# align2d/align using salign
-                                   
-from modeller import *
-log.verbose()
-env = environ()
-"""
-    tf_str_segm = ''
-
-    # set vars specific for st-se or se-se
-    if top_type == 'stse':
-        ali_type = "progressive"
-        str_dir = "env.io.atom_files_directory='%s'" % str_dir
-        gap_fctn = "gap_function=True,"
-        gap_pen_2D = "gap_penalties_2d=(%d,%d,%d,%d,%d,%d,%d,%d,%d)," \
-                     % (inputs['2D_1'], inputs['2D_2'], inputs['2D_3'],
-                        inputs['2D_4'], inputs['2D_5'], inputs['2D_6'],
-                        inputs['2D_7'], inputs['2D_8'], inputs['2D_9'])
-        align_block = "align_block=%d," % seq_count
-        dnd_file = ''
-        max_gap = "max_gap_length=%d," % inputs['max_gap']
-    else: # seq-seq
-        if top_type == 'sese_pdbs': # TODO
-            pass
-        else: # All ali entries, no need to parse PDBs for seqs
-            str_dir = ''
-        if inputs['align_type'] == 'automatic':
-            if seq_count <= 30:
-                ali_type = "tree"
-            else:
-                ali_type = "progressive"
-        else:
-             ali_type = inputs['align_type']
-        if ali_type == "tree" and seq_count > 2:
-            dnd_name = "salign.tree"
-            dnd_file = "dendrogram_file='%s'," % dnd_name
-        else:
-            dnd_file = ""
-        gap_fctn   = ''
-        gap_pen_2D = ''
-        align_block = ''
-        max_gap = ''
-
-    # common vars for str-seq and seq-seq
-    gap_pen_1D = "%f, %f" % (inputs['1D_open'], inputs['1D_elong'])
-    read_ali_line = ''
-    read_ali_line = "aln = alignment(env)"
-    for filen, alifmt in ali_files:
-        read_ali_line += "\naln.append(file='%s', align_codes='all', " \
-                         "alignment_format='%s')" % (filen, alifmt)
-
-    overhangs = inputs['overhangs']
-    improve = inputs['improve']
-    gap_gap_score = inputs['gap-gap_score']
-    gap_res_score = inputs['gap-res_score']
-
-    script += str_dir + "\n" + read_ali_line + "\n" + tf_str_segm + """
-aln.salign(rr_file='$(LIB)/as1.sim.mat',  # Substitution matrix used
-           %(align_block)s
-           alignment_type = '%(ali_type)s',
-           %(max_gap)s
-           %(gap_fctn)s
-           %(dnd_file)s
-           feature_weights = (1., 0., 0., 0., 0., 0.),
-           gap_penalties_1d = (%(gap_pen_1D)s),
-           %(gap_pen_2D)s
-           gap_gap_score = %(gap_gap_score)f,
-           gap_residue_score = %(gap_res_score)f,
-           overhang = %(overhangs)d,
-           improve_alignment = %(improve)s,
-           output='')
-aln.write(file='%(output_ali)s', alignment_format='PIR')
-""" % locals()
-    return script
-
-
-def onestep_sese(inputs, entries, adv):
-    """Main sub for one step seq-seq alignments"""
-
-    if adv:
-        if inputs['1D_open_usr'] == 'Default':
-            inputs['1D_open'] = inputs['1D_open_sese']
-        else:
-            inputs['1D_open'] = inputs['1D_open_usr']
-        if inputs['1D_elong_usr'] == 'Default':
-            inputs['1D_elong'] = inputs['1D_elong_sese']
-        else:
-            inputs['1D_elong'] = inputs['1D_elong_usr']
-    else:
-        inputs['1D_open'] = inputs['1D_open_sese']
-        inputs['1D_elong'] = inputs['1D_elong_sese']
-
-    # get str segments if not only seqs: TODO
-
-    # Arrange all uploaded files in hashes
-    ali_files = []
-    upl_strs = {}
-    seq_count = 0 # total no of seqs
-    # Any uploaded files?
-    if os.path.exists('upl_files.db'):
-        # Get uploaded files
-        b = bsddb.db.DB()
-        b.open('upl_files.db')
-        for filen, type in b.items():
-            filen = 'upload/' + filen
-            if type == 'str': #save for use in sub cp_PBDs if needed
-                upl_strs[filen] = 1
-            elif not type.endswith('st'): # skip str files
-                type_split = type.split('-')
-                seq_count += int(type_split[1])
-                ali_files.append((filen, type_split[0]))
-                # remove structure entries if any: TODO
-
-    # perform multiple tasks on ali files if not only strs
-    if entries != 'strs':
-        if inputs['upld_pseqs'] > 0:
-            file_path = 'pasted_seqs.pir'
-            ali_files.append((file_path, 'pir'))
-            seq_count += inputs['upld_pseqs']
-
-    # Create script file
-    return sese_stse_topf(inputs, ali_files, seq_count,
-                          'sese', 'seq-seq_out.ali')
-
-
-def make_sge_script(runnercls, pyscript):
+def make_sge_script(runnercls, commands):
     script = """
 date
 hostname
-/salilab/diva1/home/modeller/mod9v8 %(pyscript)s
+module load modeller/9.13
+%s
 date
-""" % locals()
+""" % "\n".join(commands)
     r = runnercls(script)
     r.set_sge_options("-o output.error -j y -l netappsali=1G -p -4 "
                       "-l h_rt=24:00:00 -r y -N salign")
     return r
 
+def run_mod(scriptname):
+    return "python %s > %s" % (scriptname, scriptname[:-3] + '.log')
+
+def make_onestep_script(runnercls, tool):
+    if tool == 'str_str':
+        scriptname = 'str-str.py'
+    else:
+        scriptname = 'seq-seq.py'
+    commands = [run_mod(scriptname)]
+    return make_sge_script(runnercls, commands)
+
+def make_twostep_script(runnercls, tool):
+    if tool == 'str_seq':
+        top_file1 = 'str-str.py'
+        top_file2 = 'seq-seq.py'
+        top_file3 = 'final_alignment.py'
+        file2fuse_1 = 'str-str_out.ali'
+        file2fuse_2 = 'seq-seq_out.ali'
+        fuse_file = 'str-seq_fuse.ali'
+    else: # 2 step seq-seq
+        top_file1 = 'seq-seq1.py'
+        top_file2 = 'seq-seq2.py'
+        top_file3 = 'profile.py'
+        file2fuse_1 = 'seq-seq_out1.ali'
+        file2fuse_2 = 'seq-seq_out2.ali'
+        fuse_file = 'prof_in.ali'
+    commands = [run_mod(top_file1), run_mod(top_file2),
+                "cat %s %s > %s" % (file2fuse_1, file2fuse_2, fuse_file),
+                run_mod(top_file3)]
+    return make_sge_script(runnercls, commands)
 
 class Job(saliweb.backend.Job):
     runnercls = saliweb.backend.SaliSGERunner
 
     def run(self):
-        parameters = read_parameters_file(open('parameters.yaml'))
-        tool = parameters['tool']
-        if tool == '1s_sese':
-            p = onestep_sese(parameters, 'seqs', False)
-            pyscript = 'seq-seq.py'
-            open(pyscript, 'w').write(p)
-            return make_sge_script(self.runnercls, pyscript)
-        elif tool == 'sese_adv':
-            if parameters['sa_feature'] == '2s_sese':
-                raise NotImplementedError("Unsupported tool type")
-            elif parameters['sa_feature'] == 'str_seq':
-                raise NotImplementedError("Unsupported tool type")
-            else: # 1 step seq-seq
-                if parameters['structures'] == 1:
-                    typ = 'seqs_and_strs'
-                else:
-                    typ = 'seqs'
-                p = onestep_sese(parameters, typ, True)
-                pyscript = 'seq-seq.py'
-                open(pyscript, 'w').write(p)
-                return make_sge_script(self.runnercls, pyscript)
+        tool = anydbm.open('inputs.db')['tool']
+        if tool in ('str_str', '1s_sese'):
+            return make_onestep_script(self.runnercls, tool)
+        elif tool in ('2s_sese', 'str_seq'):
+            return make_twostep_script(self.runnercls, tool)
         else:
-            raise NotImplementedError("Unsupported tool type")
+            raise ValueError("Tool %s not recognized" % tool)
 
 
 def get_web_service(config_file):
     db = saliweb.backend.Database(Job)
     config = saliweb.backend.Config(config_file)
     return saliweb.backend.WebService(config, db)
-
