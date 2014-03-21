068238022bff5987f474193df06bd70f8c675202
Ben Webb <ben@salilab.org>
2014-03-21 21:10:07 +0000

    Test helper methods in salign.pm.

--- test/frontend/frontend.pl
+++ test/frontend/frontend.pl
@@ -20,3 +20,25 @@ my $t = new saliweb::Test('salign');
          qr#<a href="http://modbase/top/queue.cgi">SALIGN Current queue</a>#,
          'Queue link');
 }
+
+# Test get_project_menu
+{
+    my $self = $t->make_frontend();
+    my $p = $self->get_project_menu();
+    like($p, '/Developers:.*Hannes Braberg.*Version/ms', "get_project_menu");
+}
+
+# Test get_footer
+{
+    my $self = $t->make_frontend();
+    my $p = $self->get_footer();
+    like($p, '/SALIGN : A webserver/ms', "get_footer");
+}
+
+# Test templatedir
+{
+    my $self = $t->make_frontend();
+    $self->{config}->{directories}->{install} = "/foo/bar";
+    my $t = $self->templatedir;
+    is($t, '/foo/bar/txt/template', "templatedir");
+}
