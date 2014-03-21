7b0b3d20f91ab4c056d0a604e64fb24ff14cf117
Ben Webb <ben@salilab.org>
2014-03-21 21:04:30 +0000

    Test salign::CGI_Utils.

---
+++ test/frontend/cgi_utils.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+use saliweb::Test;
+use Test::Exception;
+use Test::More 'no_plan';
+use File::Temp qw(tempdir);
+
+BEGIN {
+    use_ok('salign::CGI_Utils');
+}
+
+# Test filen_fix
+{
+    my $f = filen_fix(undef, "/foo/bar/ aA0 b-_.z ");
+    is($f, "aA0_b-_.z", "filen_fix valid file");
+
+    dies_ok { filen_fix(undef, "/foo/bar/ a#^A0 b-_.z ") }
+            "filen_fix invalid file";
+}
+
+# Test filen_fix_jr
+{
+    my $f = filen_fix_jr(undef, " aA0 b-_.z ");
+    is($f, "_aA0_b-_.z_", "filen_fix_jr valid file");
+
+    dies_ok { filen_fix_jr(undef, "a#^A0 b-_.z") }
+            "filen_fix_jr invalid file";
+}
